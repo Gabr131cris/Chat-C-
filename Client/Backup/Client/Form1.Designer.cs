404d48ef6b92e9782d9228a4636801a418b23f55
Gabriel Farcas <farcasgabriel24@yahoo.com>
2024-07-12 06:24:40 +0100

    Add files via upload

---
+++ Client/Backup/Client/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.chatBox = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.userList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(460, 10);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(75, 23);
+            this.connectBtn.TabIndex = 0;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // userBox
+            // 
+            this.userBox.Location = new System.Drawing.Point(233, 13);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(163, 20);
+            this.userBox.TabIndex = 1;
+            // 
+            // chatBox
+            // 
+            this.chatBox.Location = new System.Drawing.Point(233, 112);
+            this.chatBox.Multiline = true;
+            this.chatBox.Name = "chatBox";
+            this.chatBox.Size = new System.Drawing.Size(311, 173);
+            this.chatBox.TabIndex = 2;
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(469, 305);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 3;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            // 
+            // userList
+            // 
+            this.userList.DisplayMember = "0";
+            this.userList.FormattingEnabled = true;
+            this.userList.Location = new System.Drawing.Point(31, 13);
+            this.userList.Name = "userList";
+            this.userList.Size = new System.Drawing.Size(176, 316);
+            this.userList.TabIndex = 4;
+            this.userList.ValueMember = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(574, 357);
+            this.Controls.Add(this.userList);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.chatBox);
+            this.Controls.Add(this.userBox);
+            this.Controls.Add(this.connectBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.TextBox chatBox;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.ListBox userList;
+    }
+}
+
---
+++ Client/Backup/Client/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client = null;
+        NetworkStream stream = null;
+        StreamReader streamReader = null;
+        StreamWriter streamWriter = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+             client = new TcpClient("127.0.0.1", 8000);
+        
+             stream = client.GetStream();
+             streamReader = new StreamReader(stream);
+             streamWriter = new StreamWriter(stream);
+             streamWriter.WriteLine(userBox.Text + "#");
+             streamWriter.Flush();
+             stream.Close();
+             client.Close();
+        }
+    }
+}
